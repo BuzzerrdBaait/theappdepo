af71e1b2d16cfcd8f6d0a7952254eb67f6fe851c
Papa Perry <pappaperrys@gmail.com>
2024-08-23 02:51:10 -0500

    I am migrating with no errors, except the data is not persistant. I am taking out sqlite to see if this is why.

--- theappdepo/settings.py
+++ theappdepo/settings.py
@@ -110,18 +110,16 @@ if is_deployed:
 
 
     bucketurl='https://iloverecipes.s3.us-east-2.amazonaws.com'
+
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / "db.sqlite3",
-        }
+        "default": dj_database_url.config(conn_max_age=600, default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3"))
     }
     
              
-    #DATABASE_URL = os.environ['DATABASE_URL']
+    DATABASE_URL = os.environ['DATABASE_URL']
 
-    #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
