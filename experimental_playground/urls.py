41c77deb4d5e1640b9eff2cb75f463c033841fca
Papa Perry <pappaperrys@gmail.com>
2024-08-24 00:42:52 -0500

    starting from the beginning, removing apps and going to go through one at a time

--- experimental_playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name='experimental_playground'
-
-urlpatterns=[
-
-     path('X1', views.experiment_room_1,name='x1')
-]
--- ilovecookbooks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IlovecookbooksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ilovecookbooks'
